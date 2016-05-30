dcfce5b0b19fd68262e545c6bb07085af9df4966
John Mandalakas <john@mandalakas.com>
2016-05-30 12:29:15 -0500

    Calculate center of countries

--- app.js
+++ app.js
@@ -3,23 +3,37 @@ var express  = require('express'),
     topojson = require('topojson'),
     world    = require('./data/world.json');
 
+//extended projections
+require("d3-geo-projection")(d3);
+
 var app = express();
 app.set('view engine', 'ejs');
+
+const WIDTH = 960,
+    HEIGHT = 500;
 
 function generate(country) {
   if (!country) return {};
   var subunits = topojson.feature(world, world.objects.subunits_world);
-  var projection = d3.geo
-    .equirectangular()
-
-  var path  = d3.geo
-    .path()
-    .projection(projection);
 
   var data = subunits.features
     .filter(f => {
       return f.id === country})
     .map(f => {
+      var scale  = 500;
+      var center = d3.geo.centroid(f)
+      var offset = [WIDTH / 2, HEIGHT / 2];
+
+      var projection = d3.geo
+          .mercator()
+          .scale(scale)
+          .translate(offset)
+          .center(center)
+
+      var path = d3.geo
+        .path()
+        .projection(projection);
+
       return {
         name: f.properties.name,
         id: f.id,
@@ -36,7 +50,7 @@ app.get('/countries/:country?.svg?', (req, res) => {
     res.status(404);
     res.send({error: 404, message: 'Could not find this country'});
   } else {
-    res.render('template', {viewbox: '0 0 400 220', path:  data[0].coords[0]});
+    res.render('template', {viewbox: `0 0 ${WIDTH} ${HEIGHT}`, path:  data[0].coords[0]});
   }
 });
 
